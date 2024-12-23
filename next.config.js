5e94e398182dce317a163b8d091e5bd04c8a51a8
SuXin9716 <suxin8348@gmail.com>
2024-12-24 01:06:38 +0900

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+<antArtifact identifier="next-config" type="application/vnd.ant.code" language="javascript">
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
