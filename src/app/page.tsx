2ce4b3566c6a15a9d02dd1ee5ec5318f74adac85
SuXin9716 <suxin8348@gmail.com>
2024-12-24 01:59:22 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-<antArtifact identifier="page-tsx" type="application/vnd.ant.code">
-import TaskManagementSystem from '@/components/TaskManagementSystem'
+import TaskManagementSystem from '@/components/TaskManagementSystem';
 
 export default function Home() {
   return (
     <main>
       <TaskManagementSystem />
     </main>
-  )
+  );
 }
