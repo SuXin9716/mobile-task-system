77efd41e890cee175b58a76e465145a8048e77ec
SuXin9716 <suxin8348@gmail.com>
2024-12-24 01:12:41 +0900

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+<antArtifact identifier="page-tsx" type="application/vnd.ant.code">
+import TaskManagementSystem from '@/components/TaskManagementSystem'
+
+export default function Home() {
+  return (
+    <main>
+      <TaskManagementSystem />
+    </main>
+  )
+}
