a2ebafc2781c00b15309ff0827fe88a3c366302f
SuXin9716 <suxin8348@gmail.com>
2024-12-24 01:16:42 +0900

    Create utils.ts

---
+++ src/lib/utils.ts
@@ -0,0 +1,7 @@
+<antArtifact identifier="utils-ts" type="application/vnd.ant.code">
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
